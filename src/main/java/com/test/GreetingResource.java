0b4aebb47799a8a9101b052d70f21e077981efe3
osho81 <ossi.hour@gmail.com>
2023-11-04 10:44:21 +0100

    Initial commit

---
+++ src/main/java/com/test/GreetingResource.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/v1")
+public class GreetingResource {
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @POST
+    @Path("/addperson")
+//    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person addPerson(Person person) {
+        System.out.println("In add Person method");
+        Person tempPerson = new Person();
+        tempPerson.setFirstName(person.getFirstName());
+        tempPerson.setLastName(person.getLastName());
+        tempPerson.setAge(person.getAge());
+        Person.persist(tempPerson);
+        return person;
+    }
+}
---
+++ src/main/java/com/test/Person.java
@@ -0,0 +1,65 @@
+package com.test;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person extends PanacheEntity {
+
+//    @Id
+//    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(Long id, String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
