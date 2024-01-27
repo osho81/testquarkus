5f962224be2759f58d8ebee59a6dc36854040042
osho81 <ossi.hour@gmail.com>
2024-01-27 15:40:42 +0100

    Add deletePerson method

--- src/main/java/com/test/PersonResource.java
+++ src/main/java/com/test/PersonResource.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 // TODO: Add request response to the api methods
 // TODO: Refactore and do service methods instead
+// TODO: Add tests to the api methods in corresponding test-file
 
 @Path("/api/v1")
 public class PersonResource {
