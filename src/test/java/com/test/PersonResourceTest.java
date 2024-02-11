1f42abcc06506077a6add71caa0e9d3339091ce9
osho81 <ossi.hour@gmail.com>
2024-02-11 10:31:03 +0100

    Add mockito from org.ockito

--- src/test/java/com/test/PersonResourceTest.java
+++ src/test/java/com/test/PersonResourceTest.java
@@ -6,6 +6,7 @@ import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.*;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
